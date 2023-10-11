5b8f518f71a80e497a037fe00e0d94e8d094ea4a
Grouvie <cedo12@web.de>
2023-10-11 23:22:11 +0200

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ async fn main() {
     // build our application with a route
     let routes_all = Router::new().route("/", get(handler));
 
-    // run it
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     println!("->> LISTENING on {addr}\n");
+    // run it
     axum::Server
         ::bind(&addr)
         .serve(routes_all.into_make_service_with_connect_info::<SocketAddr>()).await
